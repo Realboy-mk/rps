e88d3f1c0180605e0e8da821dcf70c9c59d3dd2f
Realboy-mk <[matt.kline.code@gmail.com]>
2024-08-05 13:25:23 -0400

    feat: intialize js script with pseudocode

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//Get computer choice using rnadom number generator//
+
+//Get human choice using prompt function//
+
+//Declare player score variables//
+
+//wirte playRound function to play a single round of RPS//
+
+//Play a game of rps consisiting of 5 games//
