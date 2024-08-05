0072e58360b6f00f5918864a58e7f48b1922dd66
Realboy-mk <[matt.kline.code@gmail.com]>
2024-08-05 13:50:05 -0400

    feat: add function to get computer choice
    Get computer choice using random number generated less than 1.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
 //Get computer choice using rnadom number generator//
 
+function getComputerChoice(){
+    let randomNum = Math.random();
+    if(randomNum < 0.33){
+        return "rock";
+    } else if(randomNum < 0.66){
+        return "paper";
+    } else{
+        return "scissors";
+        }
+}
+
+
 //Get human choice using prompt function//
 
 //Declare player score variables//
